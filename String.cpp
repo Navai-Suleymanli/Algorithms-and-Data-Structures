76f02b1fc598f75d60bef2ad65a3b07b0a3403ea
Navai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2023-11-22 04:37:34 +0400

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main()
 {
-    // strings are also dynamic arrays, but they have speacial syntax:
+    // strings are also dynamic arrays, but they have special syntax:
     // STRING FUNCTIONS:
-    // .substr(k, x);    ----- returns the substring that begins at position k and has length x 
-    // .find(t)          ----- finds the position of the first occurrence of a substring t.
+    // .length()  & .size()  ----- returns the length of the string
+    // .swap(x)              ----- This function is used to swap the values of 2 strings
+    // .substr(k, x);        ----- returns the substring that begins at position k and has length x 
+    // .find(t)              ----- finds the position of the first occurrence of a substring t
+    // .begin(), .end()      ----- vectordaki kimi iterator funksiyalar
+    // strcat(dest, src);    ----- appends 2 strings
+    // strcopy(dest, src);   -----  copies a character string from source to destination
 
     string a = "Azerbaycan";
     string  b = a+a; // "AzerbaycanAzerbaycan"
